71fe50dbe5c94a8c03e1e70e1e425564cf6d7e77
PasinduYeshan <pasindua.18@cse.mrt.ac.lk>
2021-01-10 13:03:07 +0530

    Project Structure changed and Authentication

---
+++ src/database/database.js
@@ -0,0 +1,43 @@
+const mysql = require("mysql");
+const config = require("config");
+const _pool = new WeakMap();
+const _connectionError = new WeakMap();
+
+class Database {
+  constructor() {
+    try {
+      _pool.set(this, mysql.createPool(config.get("database_credentials")));
+      _connectionError.set(this, false);
+    } catch (ex) {
+      console.log(ex);
+      _connectionError.set(this, true);
+    }
+  }
+
+  get connectionError() {
+    return _connectionError.get(this);
+  }
+
+  //Insert new tuples into database
+  create(tableName, columns, values) {
+    return new Promise((resolve) => {
+      _pool
+        .get(this)
+        .query(
+          `INSERT INTO ?? ${columns.length === 0 ? "" : "(??)"} VALUES ${
+            typeof values[0] === "object" ? "?" : "(?)"
+          }`,
+          [tableName, columns, values],
+          (error, results, fields) => {
+            if (error) {
+              console.log(error);
+              resolve({ error: true });
+            }
+            resolve({ error: false });
+          }
+        );
+    });
+  }
+}
+
+module.exports = Database;
---
+++ src/models/user.js
@@ -0,0 +1,52 @@
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
+const Database = require("../database/database");
+const _database = new WeakMap();
+const _schema = new WeakMap();
+const _validate = new WeakMap();
+
+class User {
+  constructor() {
+    _database.set(this, new Database());
+
+    _schema.set(
+      this,
+      Joi.object({
+        first_name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(10).max(255).email().required(),
+        password: Joi.string().min(5).max(30).required(),
+      }).options({ abortEarly: false })
+    );
+
+    _validate.set(this, (object) => {
+      return _schema.get(this).validate(object);
+    });
+  }
+
+  async register(data) {
+    //validate data
+    let result = await _validate.get(this)(data);
+    if (result.error)
+      return new Promise((resolve) => resolve({ validationError: result }));
+
+    //encrypt the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(data.password, salt);
+    data.password = hashedPassword;
+
+    //call create function of database class
+    result = await _database
+      .get(this)
+      .create("users", Object.keys(data), Object.values(data));
+
+    return new Promise((resolve) => {
+      let obj = {
+        connectionError: _database.get(this).connectionError,
+      };
+      result.error ? (obj.error = true) : (obj.error = false);
+      resolve(obj);
+    });
+  }
+}
+
+module.exports = User;
