5b250566222a76d9be3c044b50b2ce64a0a21745
DilshanSenarath <dilshan99news@gmail.com>
2021-01-12 23:46:13 +0530

    Complete database class

--- src/database/database.js
+++ src/database/database.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 const config = require("config");
 const _pool = new WeakMap();
 const _connectionError = new WeakMap();
+const _getResults = new WeakMap();
 
 class Database {
   constructor() {
@@ -12,6 +13,14 @@ class Database {
       console.log(ex);
       _connectionError.set(this, true);
     }
+
+    _getResults.set(this, (error, results, fields) => {
+      if (error) {
+        console.log(error);
+        resolve({ error: true });
+      }
+      resolve({ error: false, result: results });
+    });
   }
 
   get connectionError() {
@@ -28,18 +37,109 @@ class Database {
             typeof values[0] === "object" ? "?" : "(?)"
           }`,
           [tableName, columns, values],
-          (error, results, fields) => {
-            if (error) {
-              console.log(error);
-              resolve({ error: true });
-            }
-            resolve({ error: false });
-          }
+          _getResults.get(this)(error, results, fields)
         );
     });
   }
 
-  read() {}
+  //read data from single table
+  readSingleTable(tableName, columns, action = [], sort = [], limit = []) {
+    return new Promise((resolve) => {
+      _pool
+        .get(this)
+        .query(
+          `SELECT ?? FROM ?? ${
+            action.length === 0 ? `` : `WHERE ?? ${action[1]} ?`
+          } ${sort.length === 0 ? `` : `ORDER BY ??`} ${
+            limit.length == 0 ? `` : `LIMIT ?`
+          }`,
+          [columns, tableName, action[0], action[2], sort, limit],
+          _getResults.get(this)(error, results, fields)
+        );
+    });
+  }
+
+  //read data from multiple tables using join operators
+  readMultipleTable(
+    mainTable,
+    join,
+    joiningTables,
+    columns,
+    action = [],
+    sort = [],
+    limit = []
+  ) {
+    return new Promise((resolve) => {
+      _pool
+        .get(this)
+        .query(
+          `SELECT ?? FROM ?? ${joiningTables
+            .map((e, i) =>
+              i % 2 === 0
+                ? `${join === "outer" ? `LEFT` : ``} JOIN ?? USING (??)`
+                : ``
+            )
+            .join("")} ${
+            action.length === 0 ? `` : `WHERE ?? ${action[1]} ?`
+          } ${sort.length === 0 ? `` : `ORDER BY ??`} ${
+            limit.length == 0 ? `` : `LIMIT ?`
+          }`,
+          [
+            columns,
+            mainTable,
+            ...joiningTables,
+            action[0],
+            action[2],
+            sort,
+            limit,
+          ],
+          _getResults.get(this)(error, results, fields)
+        );
+    });
+  }
+
+  //update a table
+  update(tableName, updates, action = []) {
+    return new Promise((resolve) => {
+      _pool
+        .get(this)
+        .query(
+          `UPDATE ?? SET ${updates
+            .map((e, i) =>
+              i % 2 === 0 ? `?? = ?` : `${i === updates.length - 1 ? `` : `,`}`
+            )
+            .join("")} WHERE ?? ${action[1]} ?`,
+          [tableName, ...updates, action[0], action[2]],
+          _getResults.get(this)(error, results, fields)
+        );
+    });
+  }
+
+  //delete tuples from a table
+  delete(tableName, action) {
+    return new Promise((resolve) => {
+      _pool
+        .get(this)
+        .query(
+          `DELETE FROM ?? WHERE ?? ${action[1]} ?`,
+          [tableName, action[0], action[2]],
+          _getResults.get(this)(error, results, fields)
+        );
+    });
+  }
+
+  //for call the stored procedures
+  call(name, args = []) {
+    return new Promise((resolve) => {
+      _pool
+        .get(this)
+        .query(
+          `CALL ??(?)`,
+          [name, args],
+          _getResults.get(this)(error, results, fields)
+        );
+    });
+  }
 }
 
 module.exports = Database;
