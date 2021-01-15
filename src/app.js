7ca23bc399f26555323f2bbe8f6c7f4b751c221d
charuka <charuka.18@cse.mrt.ac.lk>
2021-01-15 18:27:36 +0530

    Update For Emission Test Report

--- src/app.js
+++ src/app.js
@@ -7,12 +7,18 @@ const reportRouter = require("./routes/emission_report");
 const path = require('path');
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname,"../public/")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/register", authRouter);
 
 app.use("/report", reportRouter);
+
+
+
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/database/database.js
+++ src/database/database.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 const config = require("config");
 const _pool = new WeakMap();
 const _connectionError = new WeakMap();
+const _getResults = new WeakMap();
 
 class Database {
   constructor() {
@@ -12,6 +13,14 @@ class Database {
       console.log(ex);
       _connectionError.set(this, true);
     }
+
+    _getResults.set(this, (error, results) => {
+      if (error) {
+        console.log(error);
+        return { error: true };
+      }
+      return { error: false, result: results };
+    });
   }
 
   get connectionError() {
@@ -29,13 +38,114 @@ class Database {
           }`,
           [tableName, columns, values],
           (error, results, fields) => {
-            if (error) {
-              console.log(error);
-              resolve({ error: true });
-            }
-            resolve({ error: false });
+            resolve(_getResults.get(this)(error, results));
           }
         );
+    });
+  }
+
+  //read data from single table
+  readSingleTable(tableName, columns, action = [], sort = [], limit = []) {
+    return new Promise((resolve) => {
+      _pool
+        .get(this)
+        .query(
+          `SELECT ?? FROM ?? ${
+            action.length === 0 ? `` : `WHERE ?? ${action[1]} ?`
+          } ${sort.length === 0 ? `` : `ORDER BY ??`} ${
+            limit.length == 0 ? `` : `LIMIT ?`
+          }`,
+          [columns, tableName, action[0], action[2], sort, limit],
+          (error, results, fields) => {
+            resolve(_getResults.get(this)(error, results));
+          }
+        );
+    });
+  }
+
+  //read data from multiple tables using join operators
+  readMultipleTable(
+    mainTable,
+    join,
+    joiningTables,
+    columns,
+    action = [],
+    sort = [],
+    limit = []
+  ) {
+    return new Promise((resolve) => {
+      _pool
+        .get(this)
+        .query(
+          `SELECT ?? FROM ?? ${joiningTables
+            .map((e, i) =>
+              i % 2 === 0
+                ? `${join === "outer" ? `LEFT` : ``} JOIN ?? USING (??)`
+                : ``
+            )
+            .join("")} ${
+            action.length === 0 ? `` : `WHERE ?? ${action[1]} ?`
+          } ${sort.length === 0 ? `` : `ORDER BY ??`} ${
+            limit.length == 0 ? `` : `LIMIT ?`
+          }`,
+          [
+            columns,
+            mainTable,
+            ...joiningTables,
+            action[0],
+            action[2],
+            sort,
+            limit,
+          ],
+          (error, results, fields) => {
+            resolve(_getResults.get(this)(error, results));
+          }
+        );
+    });
+  }
+
+  //update a table
+  update(tableName, updates, action = []) {
+    return new Promise((resolve) => {
+      _pool
+        .get(this)
+        .query(
+          `UPDATE ?? SET ${updates
+            .map((e, i) =>
+              i % 2 === 0 ? `?? = ?` : `${i === updates.length - 1 ? `` : `,`}`
+            )
+            .join("")} WHERE ?? ${action[1]} ?`,
+          [tableName, ...updates, action[0], action[2]],
+          (error, results, fields) => {
+            resolve(_getResults.get(this)(error, results));
+          }
+        );
+    });
+  }
+
+  //delete tuples from a table
+  delete(tableName, action) {
+    return new Promise((resolve) => {
+      _pool
+        .get(this)
+        .query(
+          `DELETE FROM ?? WHERE ?? ${action[1]} ?`,
+          [tableName, action[0], action[2]],
+          (error, results, fields) => {
+            resolve(_getResults.get(this)(error, results));
+          }
+        );
+    });
+  }
+
+  //for call the stored procedures
+  call(name, args = []) {
+    return new Promise((resolve) => {
+      _pool
+        .get(this)
+        .query(`CALL ??(?)`, [name, args], (error, results, fields) => {
+          resolve(_getResults.get(this)(error, results));
+        });
     });
   }
 }
