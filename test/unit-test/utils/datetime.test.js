d3a4b4160e2b7a81365cb214ed3daa1aaa7beb3d
LahiruKumaraHewagama <lahiruhewagama98@gmail.com>
2021-02-26 18:14:31 +0530

    pull yeshan

--- test/unit-test/utils/datetime.test.js
+++ test/unit-test/utils/datetime.test.js
@@ -6,9 +6,6 @@ describe("getDate", () => {
     const result = datetime();
     expect(result).toContain("2021-2-25");
 
-<<<<<<< HEAD
     expect(result).toEqual(expect.stringMatching(/^2\d{3}(-\d{1,2}){2} \d{1,2}(:\d{1,2}){2}/));
-=======
->>>>>>> test-lahiru
   });
 });
